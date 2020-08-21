694579fe286d838419a7c875d9bf3795a7f6408e
sntwandy <sntwandy@gmail.com>
2020-08-20 20:31:41 -0400

    Preparing the API to production

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ const { graphqlHTTP } = require('express-graphql');
 const { readFileSync } = require('fs');
 const { join } = require('path');
 const resolvers = require('./lib/resolvers');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const isDev = process.env.NODE_ENV !== 'production';
 
 // Schema
 const typeDefs = readFileSync(join(__dirname, '/lib', 'schema.graphql'), 'utf-8');
@@ -15,10 +17,12 @@ const schema = makeExecutableSchema({ typeDefs, resolvers });
 // Configure resolvers
 //const resolver = resolvers;
 
+app.use(cors());
+
 app.use('/api', graphqlHTTP({
   schema: schema,
   rootValue: resolvers,
-  graphiql: true,
+  graphiql: isDev,
 }));
 
 app.listen(port, () => {
